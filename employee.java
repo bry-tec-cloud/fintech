8aefc8f973c951e7c4becbf4f2700f6bec058517
Berrylloustech <badmusadeshina47@gmail.com>
2024-11-23 14:13:48 +0100

    updated employee file

--- employee.java
+++ employee.java
@@ -2,4 +2,4 @@ This file contain the status of the employee working in tne company
 This is the line included
 This line is from stage
 list the no of the employee
-
+the number of employee is five.
